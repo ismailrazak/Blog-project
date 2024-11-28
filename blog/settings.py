858001b72098706a4155551d25ffb51b51b8baa8
ismail <ismailrazak009@gmail.com>
2024-11-28 19:03:31 +0530

    Use Postgres as default db

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+import dj_database_url
+from  decouple import config
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER=config('EMAIL_HOST_USER')
 EMAIL_PASSWORD=config('EMAIL_PASSWORD')
@@ -85,13 +86,16 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+"default":
+    dj_database_url.config(default=config("DATABASE_URL"))
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
